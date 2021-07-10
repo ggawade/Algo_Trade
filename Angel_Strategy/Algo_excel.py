d608d3d2ef2bf35dd8f4cab15e8785e105592b7f
ggawade <87227454+ggawade@users.noreply.github.com>
2021-07-10 15:38:33 +0530

    Add files via upload

---
+++ Angel_Strategy/Algo_excel.py
@@ -0,0 +1,61 @@
+import angel_common_functions as f
+from xlpython import xlarg, xlfunc
+import pandas as pd
+
+
+# nfo_df = f.get_nfo_df()
+# sym_list = f.get_exch_sym_tkn_details()
+
+@xlfunc
+# @xlarg("symbol", "nparray", 2)
+def get_symbol_details(symbol):
+    nfo_df = f.get_nfo_df()
+    sym_list = f.get_exch_sym_tkn_details(symbol, nfo_df)
+    ret_str = ""
+    for i in sym_list:
+        ret_str = ret_str + str(i) + ","
+    return ret_str
+
+
+@xlfunc
+def generate_nifty_nfo_instruments():
+    f.generate_nfo_instruments()
+    return "Done!"
+
+
+@xlfunc
+def place_order(symbol, symbol_token, trans_type, exch, qty,client_id):
+    obj = f.angel_login()
+    symbol_token = str(int(symbol_token))
+    qty = str(int(qty))
+    symbol = str(symbol)
+    trans_type = str(trans_type)
+    exch = str(exch)
+    order_param = f.generate_order_param(symbol, symbol_token, trans_type, exch, qty)
+    # print(order_param)
+    # order_id = ""
+    order_id = f.place_Order(obj, order_param)
+    # order_status = "success"
+    # traded_price = '1.0'
+    if order_id != '-1':
+        order_book = f.get_order_book(obj)
+        order_df = pd.DataFrame(order_book)
+        order_details = order_df[order_df['orderid'] == order_id]
+        traded_price = '0.0'
+        if order_details['orderid'].count() == 1:
+            traded_price = str(order_details.iloc[0]['averageprice'])
+            order_status = str(order_details.iloc[0]['orderstatus'])
+    else:
+        order_status = "Failed"
+        traded_price = '-1'
+    obj.terminateSession(client_id)
+    return str(order_id) + "," + order_status + "," + traded_price
+
+
+@xlfunc
+def get_ltp(exchange, symbol, symtoken,client_id):
+    ltp_val = 0.0
+    obj = f.angel_login()
+    ltp_val = f.get_ltp(obj, exchange, symbol, symtoken)
+    obj.terminateSession(client_id)
+    return str(ltp_val)
---
+++ Angel_Strategy/Check_m2m.py
@@ -0,0 +1,65 @@
+import angel_common_functions as f
+import xlwings as xl
+from time import sleep
+import os, sys
+
+script_path = os.path.dirname(sys.argv[0])
+
+stop_fname = script_path + '/stop.txt'
+stop_loss_fname = script_path + '/stop_loss.txt'
+
+wb = xl.Book(script_path + '/Algo_excel.xlsm')
+sht = wb.sheets['Sheet1']
+stop_loss = float(sht['Q1'].value)
+print("Stop Loss Update: " + str(stop_loss))
+obj = f.angel_login()
+
+stop = os.path.isfile(stop_fname)
+if stop is True:
+    os.remove(stop_fname)
+
+while True:
+    sleep(0.5)
+
+    file_exists = os.path.isfile(stop_loss_fname)
+    if file_exists is True:
+        try:
+            sl_file = open(stop_loss_fname)
+            stop_loss = float(sl_file.readline().strip())
+            print(str(stop_loss))
+            sl_file.close()
+            os.remove(stop_loss_fname)
+            print("Stop Loss Update: " + str(stop_loss))
+        except IOError:
+            print("IOError")
+        except ValueError:
+            print("ValueError")
+        except Exception:
+            print("Issue")
+
+
+    stop = os.path.isfile(script_path + '/stop.txt')
+    if stop is True:
+        print("Exiting!!!")
+        break
+
+    pos = obj.position()['data']
+    total_pnl = 0
+    for p in pos:
+        if p['instrumenttype'] == "OPTIDX":
+            try:
+                pnl = float(p['pnl'])
+                total_pnl += pnl
+            except Exception as e:
+                break
+
+    print(total_pnl)
+
+    # if total_pnl < stop_loss:
+    #     # order_list = f.exit_all_positions(obj, pos)
+    #     order_list = ()
+    #     print('positions exited!')
+    #     print(order_list)
+    #     break
+
+obj.terminateSession("G53356")
